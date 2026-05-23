7d445a93fb32b13bff35e26b61f333762a9c86c6
Arun R <arunmahi921@gmail.com>
2026-05-23 10:23:47 +0530

    issue resolved for frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://phone-verification-delta.vercel.app/"
+      "https://phone-verification-delta.vercel.app"
     ],
     credentials: true
   })
